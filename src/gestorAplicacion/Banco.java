50f8aa0cd91a9ba0e834a06f4fdec6df5ce6364e
JackelinBlanquiceth <glozanob@unal.edu.co>
2025-01-01 09:49:13 -0500

    getters y setters

--- src/gestorAplicacion/Banco.java
+++ src/gestorAplicacion/Banco.java
@@ -2,9 +2,55 @@ package gestorAplicacion;
 import java.util.ArrayList;
 
 public class Banco {
-	String nombreCuenta;
-	ArrayList<Deuda> deuda;
-	long ahorroBanco;
-	int interes;
-	static ArrayList<Banco> listaBancos;
+	private String nombreCuenta;
+	private ArrayList<Deuda> deuda;
+	private long ahorroBanco;
+	private int interes;
+	private static ArrayList<Banco> listaBancos;
+
+	public String getNombreCuenta() {
+		return nombreCuenta;
+	}
+	
+	public ArrayList<Deuda> getDeuda() {
+		return deuda;
+	}
+	
+	public long getAhorroBanco() {
+		return ahorroBanco;
+	}
+	
+	public int getInteres() {
+		return interes;
+	}
+	
+	public ArrayList<Banco> getListaBancos() {
+		return listaBancos;
+	}
+	
+	public void setNombreCuenta(String nombreCuenta) {
+		this.nombreCuenta = nombreCuenta;
+	}
+	
+	public void setDeuda(ArrayList<Deuda> deuda) {
+	  if (deuda == null) {
+            throw new IllegalArgumentException("La lista no puede ser nula");
+        }
+        this.deuda = deuda;
+	}
+	
+	public void setAhorroBanco(long ahorroBanco) {
+		this.ahorroBanco = ahorroBanco;
+	}
+	
+	public void setInteres(int interes) {
+		this.interes = interes;
+	}
+	
+	public void setListaBancos(ArrayList<Banco> listaBancos) {
+		  if (listaBancos == null) {
+	            throw new IllegalArgumentException("La lista no puede ser nula");
+	        }
+	        this.listaBancos = listaBancos;
+		}
 }
--- src/gestorAplicacion/Bolsa.java
+++ src/gestorAplicacion/Bolsa.java
@@ -2,6 +2,14 @@ package gestorAplicacion;
 import java.util.ArrayList;
 
 public class Bolsa extends Insumo {
+	private int apacidadMaxima;
 	
+	public int getApacidadMaxima() {
+		return apacidadMaxima;
+	}
+	
+	public void setApacidadMaxima(int apacidadMaxima) {
+		this.apacidadMaxima = apacidadMaxima;
+	}
 }
 
