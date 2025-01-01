7851e728ff6d62afb34fc6b8f9806bce956bdf4f
Andres David Calderon <david.voidgames@gmail.com>
2024-12-31 21:52:15 -0500

    implementar medidas de rendimiento

--- src/gestorAplicacion/Empleado.java
+++ src/gestorAplicacion/Empleado.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 public class Empleado extends Persona{
 	
 	public static ArrayList <Empleado> listaEmpleados;
+    ArrayList<Integer> balances = new ArrayList<Integer>();
+    // Usado para empleados de Dirección.
+
     private Area areaActual;
     String fechaContratacion;
     //LocalDate?
@@ -12,16 +15,56 @@ public class Empleado extends Persona{
     Maquinaria maquinaria;
     ArrayList<Area> areas;
     int traslados;
+    int prendasArruinadas=0;
+    int prendasProducidas=0;
     
     Empleado(){
     	listaEmpleados.add(this);
     }
     
-    public ArrayList <Empleado> despedirEmpleado(){
+    public ArrayList <Empleado> listaInicialDespedirEmpleado(){
         ArrayList <Empleado> listaADespedir = null;
         for (Sede sede : Sede.listaSedes){
             for (Empleado emp : sede.listaEmpleado){
-                
+                int rendimiento;
+                switch (emp.areaActual){
+                    case CORTE:
+                        rendimiento = (emp.prendasProducidas/emp.prendasArruinadas)*100;
+                        break;
+                    case VENTAS:
+                        float acumuladoVentasSede = 0;
+                        for (Empleado empAcumulado : sede.listaEmpleado){
+                            if (empAcumulado.areaActual == Area.VENTAS){
+                                acumuladoVentasSede+=Venta.acumuladoVentasAsesoradas(empAcumulado);
+                            }
+                        }
+                        float promedioVentasSede = acumuladoVentasSede/sede.listaEmpleado.size();
+                        rendimiento = (int) ((Venta.acumuladoVentasAsesoradas(emp)/promedioVentasSede)*100);
+                        break;
+                    case OFICINA:
+                        acumuladoVentasSede = 0;
+                        for (Empleado empAcumulado : sede.listaEmpleado){
+                            if (empAcumulado.areaActual == Area.VENTAS){
+                                Venta.acumuladoVentasEmpleadoEncargado(emp);
+                            }
+                        }
+                        promedioVentasSede = acumuladoVentasSede/sede.listaEmpleado.size();
+                        rendimiento = (int) ((Venta.acumuladoVentasAsesoradas(emp)/promedioVentasSede)*100);
+                        break;
+
+                    case DIRECCION:
+                        int balancesPositivos = 0;
+                        int balancesNegativos = 0;
+                        for (int balance : emp.balances){
+                            if (balance > 0){
+                                balancesPositivos++;
+                            } else {
+                                balancesNegativos++;
+                            }
+                        }
+                        rendimiento = (balancesPositivos/(balancesNegativos+balancesPositivos))*100;
+                        break;
+                    }
             }
         }
         return listaADespedir;
--- src/gestorAplicacion/Venta.java
+++ src/gestorAplicacion/Venta.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Venta {
 	ArrayList<Prenda> articulos;
 	ArrayList<Integer> cantidades;
-	Empleado engargado;
+	Empleado encargado;
 	Empleado asesor;
 	Sede sede;
 	String fechaVenta;
@@ -14,4 +14,25 @@ public class Venta {
 	int numero; 
 	int costoEnvio;
 	int subtotal;
+
+	// Metodo ayudante para Empleado.listaInicialDespedirEmpleado,
+	// que calcula el acumulado de ventas asesoradas o registradas por empleado en pesos.
+	static public int acumuladoVentasAsesoradas(Empleado empleado){
+		int acumulado=0;
+		for (Venta venta : empleado.sede.historialVentas){
+			if (venta.asesor == empleado){
+				acumulado += venta.subtotal;;
+			}
+		}
+		return acumulado;
+	}	
+	static public int acumuladoVentasEmpleadoEncargado(Empleado empleado){
+		int acumulado=0;
+		for (Venta venta : empleado.sede.historialVentas){
+			if (venta.encargado == empleado){
+				acumulado += venta.subtotal;;
+			}
+		}
+		return acumulado;
+	}
 }
