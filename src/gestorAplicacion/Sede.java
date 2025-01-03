eef717b49872c04ace686178e5644b7531eaf3b7
Andres David Calderon <david.voidgames@gmail.com>
2025-01-03 16:14:57 -0500

    primera parte de interaccion 2 gestion humana

--- src/gestorAplicacion/Sede.java
+++ src/gestorAplicacion/Sede.java
@@ -1,4 +1,5 @@
 package gestorAplicacion;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 public class Sede{
 	private static ArrayList<Sede> listaSedes;
@@ -41,8 +42,67 @@ public class Sede{
 	public int getDistancia(){return distancia;}
 	public void setDistancia(int distancia){this.distancia=distancia;}	
 
-	public void reorganizarEmpleados(ArrayList<Empleado> despedidos){
-		// Reemplaza a los empleados despedidos
-		ArrayList<Empleado> porReemplazar = (ArrayList<Empleado>) despedidos.clone();
+	
+	// Retorna una lista con los roles a reemplazar y que sede debe reemplazar los empleados de cada rol, de ser posible. Luego también la cantidad de empleados que quedan por reemplazar.
+	static public ArrayList<Object> obtenerNececidadTransferenciaEmpleados(ArrayList<Empleado> despedidos){ //Despedidos es A en el doc.
+		ArrayList<Sede> transferirDe = new ArrayList<Sede>();
+		ArrayList<Rol> rolesATransferir = new ArrayList<Rol>();
+
+		for (int idxRol=0; idxRol<rolesATransferir.size();idxRol++){
+			Rol rol = Rol.values()[idxRol];
+			for (Sede sede : listaSedes){
+				switch(rol){
+					case MODISTA:
+					int produccionTotal = 0;
+					for (int nProduccion: sede.getProduccionAproximada()){
+						produccionTotal += nProduccion;
+					}
+
+					int produccionPorModista = produccionTotal / sede.cantidadPorRol(rol);
+
+					if (produccionPorModista<30){
+						transferirDe.set(idxRol, sede);
+						rolesATransferir.add(rol);
+						break;
+					}
+					break;
+					
+					case SECRETARIA:
+					int ejecutivos = sede.cantidadPorRol(Rol.EJECUTIVO);
+					int secretarias = sede.cantidadPorRol(Rol.SECRETARIA);
+					int empleados = sede.listaEmpleado.size();
+					if (!(empleados/secretarias > 18 || ejecutivos/secretarias > 2)){
+						transferirDe.set(idxRol, sede);
+						rolesATransferir.add(rol);
+						break;
+					}
+				}
+			}
+		}
+		ArrayList<Object> retorno= new ArrayList<Object>();
+		retorno.add(rolesATransferir);
+		retorno.add(transferirDe);
+
+		ArrayList<Empleado> aReemplazar = (ArrayList) despedidos.clone();
+		for (Empleado emp : despedidos){
+			if (rolesATransferir.contains(emp.getRol())){
+				aReemplazar.remove(emp);
+			}
+		}
+
+		retorno.add(aReemplazar);
+		return retorno;
+	}
+
+	// Devuelve la cantidad de empleados que hay en la sede con el rol dado
+	// metodo ayudante para reorganizarEmpleados
+	public int cantidadPorRol(Rol rol){
+		int cantidad = 0;
+		for (Empleado emp : listaEmpleado) {
+			if (emp.getRol() == rol){
+				cantidad++;
+			}
+		}
+		return cantidad;
 	}
 }
