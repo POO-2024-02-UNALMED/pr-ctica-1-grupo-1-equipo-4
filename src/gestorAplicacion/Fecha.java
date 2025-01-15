4789a1b79151d911cec74e9afb3ccbc500630b92
LuisRincon16 <lurinconj@unal.edu.co>
2025-01-14 21:38:30 -0500

    clase Persona y Fecha

---
+++ src/gestorAplicacion/Fecha.java
@@ -0,0 +1,51 @@
+package gestorAplicacion;
+
+public class Fecha{
+	private int dia;
+	private int mes;
+	private int año;
+
+	public Fecha(){}
+	public Fecha(int dia, int mes, int año){this.dia=dia;this.mes=mes;this.año=año;}
+
+	public int getDia() {return dia;}
+	public void setDia(int dia) {this.dia=dia;}
+	public int getMes() {return mes;}
+	public void setMes(int mes) {this.mes=mes;}
+	public int getAño() {return año;}
+	public void setaAño(int año) {this.año=año;}
+	
+	public static boolean compararAño(int año1, int año2) {
+		if (año1==año2){
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean compararMes(int mes1,int mes2) {
+		if (mes1==mes2){
+			return true;
+		}
+		return false;
+	}
+	public static boolean compararDia(int dia1, int dia2) {
+		if (dia1==dia2){
+			return true;
+		}
+		return false;
+	}
+
+	public Fecha restarMeses(int meses) {
+		int mes = this.mes - meses;
+		int año = this.año;
+		if (mes < 1) {
+			mes += 12;
+			año--;
+		}
+		return new Fecha(this.dia, mes, año);
+	}
+
+	public static boolean compararFecha(Fecha fecha1, Fecha fecha2) {
+		return (compararAño(fecha1.año, fecha2.año) && compararMes(fecha1.mes, fecha2.mes) && compararDia(fecha1.dia, fecha2.dia));
+	}
+}
---
+++ src/gestorAplicacion/Persona.java
@@ -0,0 +1,93 @@
+package gestorAplicacion;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import gestorAplicacion.Administracion.Empleado;
+import gestorAplicacion.Administracion.Rol;
+import gestorAplicacion.Bodega.Maquinaria;
+
+public class Persona implements Serializable{
+
+	
+	private static final long serializarVersionUID = 2L; // Para serializacion
+	
+	protected static ArrayList <Persona> listaPersonas = new ArrayList<Persona>();
+	protected String nombre;
+	protected int documento;
+	protected int salario;
+	protected Rol rol;
+	protected int experiencia;
+	protected boolean trabaja;
+	protected Membresia membresia;
+	
+	Persona(){
+		listaPersonas.add(this);
+	}
+	public Persona (String nom, int doc, Rol rol, int exp, boolean t, Membresia mem){
+		this.nombre=nom;
+		this.documento=doc;
+		this.rol=rol;
+		this.experiencia=exp;
+		this.trabaja=t;
+		this.membresia=mem;
+		listaPersonas.add(this);
+	}
+
+	public Rol getRol(){return rol;}
+	public Membresia getMembresia(){return membresia;}
+	public int getDocumento(){return documento;}
+	public String getNombre(){return nombre;}
+	public int getExperiencia(){return experiencia;}
+	public boolean isTrabaja(){return trabaja;}
+	public static ArrayList<Persona> getListaPersonas(){return listaPersonas;}
+	public static void setListaPersonas(ArrayList<Persona> lista){listaPersonas=lista;} // Usado al deserializear
+
+	// Interacción 3 de Gestion Humana
+	// Retorna la lista de aptos, la lista de roles y cuantos de cada rol.
+	public static ArrayList<Object> entrevistar(ArrayList<Empleado> aReemplazar){
+		ArrayList<Rol> rolesAReemplazar = new ArrayList<Rol>();
+		ArrayList<Integer> cantidad = new ArrayList<Integer>();
+		for(Empleado empleado: aReemplazar){
+			if(!rolesAReemplazar.contains(empleado.getRol())){
+				rolesAReemplazar.add(empleado.getRol());
+				cantidad.add(0);
+			}
+			int rolIdx = rolesAReemplazar.indexOf(empleado.getRol());
+			cantidad.set(rolIdx, cantidad.get(rolIdx)+1);
+		}
+
+		ArrayList<Persona> aptos = new ArrayList<Persona>();
+
+		for (Persona persona:listaPersonas){
+			if(persona.trabaja==false && rolesAReemplazar.contains(persona.getRol())){
+				aptos.add(persona);
+				break;
+			}
+		}
+
+		ArrayList<Object> retorno = new ArrayList<Object>();
+		retorno.add(aptos);
+		retorno.add(rolesAReemplazar);
+		retorno.add(cantidad);
+		return retorno;
+	}
+
+	// Interacción 3 de Gestion Humana
+	static public void contratar(ArrayList<Persona> aContratar, ArrayList<Empleado> aReemplazar){
+		for(Persona persona: aContratar){
+			Persona.listaPersonas.remove(persona);
+			Empleado emp = new Empleado(persona);
+			for(Empleado antiguo: aReemplazar){
+				if(antiguo.getRol().equals(persona.getRol())){
+					emp.setAreaActual(antiguo.getAreaActual());
+					emp.setSede(antiguo.getSede());
+					Maquinaria.asignarMaquinaria(emp);
+					aReemplazar.remove(antiguo);
+					break;
+				}
+			}
+		}
+	}
+
+}
